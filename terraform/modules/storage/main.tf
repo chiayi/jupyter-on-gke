2a22bf0e446043865d72800b849e323bc9f11a0c
Aaron Liang <aaronliang@google.com>
2023-08-01 11:03:16 -0700

    Removed force destroy from storage modules

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -2,5 +2,4 @@ resource "google_storage_bucket" "persist_storage" {
   count         = var.include_module_storage ? 1 : 0
   name          = "${var.project_id}-bucket"
   location      = var.region
-  force_destroy = true
 }
